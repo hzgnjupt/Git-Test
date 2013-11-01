d7e50412fa37b604a2279012591891860dc45f22
hzgnjupt <hzgnjupt@gmail.com>
2013-11-01 12:03:58 +0800

    test!

---
+++ test!/src/App.java
@@ -0,0 +1,45 @@
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.util.Random;
+
+public class App {
+
+    public static void main(String[] args) throws AWTException, InterruptedException {
+
+        Random  ran = new Random();
+        
+        Robot robot = new Robot();
+        while (true) {
+            Thread.sleep(ran.nextInt(5)*1000);
+            robot.keyPress(KeyEvent.VK_5);
+            robot.keyRelease(KeyEvent.VK_5);
+            Thread.sleep(ran.nextInt(10)*1000);
+            robot.keyPress(KeyEvent.VK_E);
+            robot.keyRelease(KeyEvent.VK_E);
+            Thread.sleep(ran.nextInt(5)*1000);
+            robot.keyPress(KeyEvent.VK_4);
+            robot.keyRelease(KeyEvent.VK_4);
+            Thread.sleep(ran.nextInt(5)*1000);
+            robot.keyPress(KeyEvent.VK_F);
+            robot.keyRelease(KeyEvent.VK_F);
+            Thread.sleep(3000);
+            robot.keyPress(KeyEvent.VK_SHIFT);
+            robot.keyPress(KeyEvent.VK_1);
+            robot.keyRelease(KeyEvent.VK_1);
+            robot.keyRelease(KeyEvent.VK_SHIFT);
+            Thread.sleep(2000);
+            robot.keyPress(KeyEvent.VK_SHIFT);
+            robot.keyPress(KeyEvent.VK_3);
+            robot.keyRelease(KeyEvent.VK_3);
+            robot.keyRelease(KeyEvent.VK_SHIFT);
+            Thread.sleep(2000);
+            robot.keyPress(KeyEvent.VK_SHIFT);
+            robot.keyPress(KeyEvent.VK_Q);
+            robot.keyRelease(KeyEvent.VK_Q);
+            robot.keyRelease(KeyEvent.VK_SHIFT);
+        }
+
+    }
+
+}
