3f6d1e0c61c294d392840137a0cf20b9746fdd56
hzgnjupt <hzgnjupt@gmail.com>
2013-11-01 14:21:43 +0800

    1111

--- test!/src/App.java
+++ test!/src/App.java
@@ -5,42 +5,42 @@ import java.util.Random;
 
 public class App {
 
-    public static void main(String[] args) throws AWTException, InterruptedException {
+	public static void main(String[] args) throws AWTException,
+			InterruptedException {
 
-        Random  ran = new Random();
-        
-        Robot robot = new Robot();
-        while (true) {
-            Thread.sleep(ran.nextInt(5)*1000);
-            robot.keyPress(KeyEvent.VK_5);
-            robot.keyRelease(KeyEvent.VK_5);
-            Thread.sleep(ran.nextInt(10)*1000);
-            robot.keyPress(KeyEvent.VK_E);
-            robot.keyRelease(KeyEvent.VK_E);
-            Thread.sleep(ran.nextInt(5)*1000);
-            robot.keyPress(KeyEvent.VK_4);
-            robot.keyRelease(KeyEvent.VK_4);
-            Thread.sleep(ran.nextInt(5)*1000);
-            robot.keyPress(KeyEvent.VK_F);
-            robot.keyRelease(KeyEvent.VK_F);
-            Thread.sleep(3000);
-            robot.keyPress(KeyEvent.VK_SHIFT);
-            robot.keyPress(KeyEvent.VK_1);
-            robot.keyRelease(KeyEvent.VK_1);
-            robot.keyRelease(KeyEvent.VK_SHIFT);
-            Thread.sleep(2000);
-            robot.keyPress(KeyEvent.VK_SHIFT);
-            robot.keyPress(KeyEvent.VK_3);
-            robot.keyRelease(KeyEvent.VK_3);
-            robot.keyRelease(KeyEvent.VK_SHIFT);
-            Thread.sleep(2000);
-            robot.keyPress(KeyEvent.VK_SHIFT);
-            robot.keyPress(KeyEvent.VK_Q);
-            robot.keyRelease(KeyEvent.VK_Q);
-            robot.keyRelease(KeyEvent.VK_SHIFT);
-        }
-        Sysout.out.println("Finish");
+		Random ran = new Random();
 
-    }
+		Robot robot = new Robot();
+		while (true) {
+			Thread.sleep(ran.nextInt(5) * 1000);
+			robot.keyPress(KeyEvent.VK_5);
+			robot.keyRelease(KeyEvent.VK_5);
+			Thread.sleep(ran.nextInt(10) * 1000);
+			robot.keyPress(KeyEvent.VK_E);
+			robot.keyRelease(KeyEvent.VK_E);
+			Thread.sleep(ran.nextInt(5) * 1000);
+			robot.keyPress(KeyEvent.VK_4);
+			robot.keyRelease(KeyEvent.VK_4);
+			Thread.sleep(ran.nextInt(5) * 1000);
+			robot.keyPress(KeyEvent.VK_F);
+			robot.keyRelease(KeyEvent.VK_F);
+			Thread.sleep(3000);
+			robot.keyPress(KeyEvent.VK_SHIFT);
+			robot.keyPress(KeyEvent.VK_1);
+			robot.keyRelease(KeyEvent.VK_1);
+			robot.keyRelease(KeyEvent.VK_SHIFT);
+			Thread.sleep(2000);
+			robot.keyPress(KeyEvent.VK_SHIFT);
+			robot.keyPress(KeyEvent.VK_3);
+			robot.keyRelease(KeyEvent.VK_3);
+			robot.keyRelease(KeyEvent.VK_SHIFT);
+			Thread.sleep(2000);
+			robot.keyPress(KeyEvent.VK_SHIFT);
+			robot.keyPress(KeyEvent.VK_Q);
+			robot.keyRelease(KeyEvent.VK_Q);
+			robot.keyRelease(KeyEvent.VK_SHIFT);
+		}
+
+	}
 
 }
