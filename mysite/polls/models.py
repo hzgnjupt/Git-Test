0a9ece7c816f0a63a7cdc603ea5e4beff4b5d394
hzgnjupt <hzgnjupt@gmail.com>
2013-11-28 09:22:21 +0800

    添加测试用例

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -10,12 +10,20 @@ class Poll(models.Model):
     pub_date = models.DateTimeField('date published')
     
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return timezone.now() - datetime.timedelta(days=1) <= self.pub_date < now
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
+    
+    def __str__(self):
+        return self.question
 
 class Choice(models.Model):
+    
     poll = models.ForeignKey(Poll)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.choice_text
